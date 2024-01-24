76c13976b6c5c8424f4ec3510aa06a3d404b9e8d
pebalap <147577666+pebalap@users.noreply.github.com>
2024-01-24 08:27:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+ <head>
+
+<title>Hacked by ./Kecot</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="description" content="Two Brothers">
+<meta name="keywords" content="Two Brothers">
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="index,cache">
+<meta name="theme-color" content="black">
+</head>
+<center>
+<body bgcolor="black">
+<div style="height: auto; min-height: 100%;">
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;">
+<i><b>
+<img src="https://i.ibb.co/WzQD6TH/girl-hentai-anime.jpg" width="240" height="240">
+<br></font><font color="lime" size="6" face="iceland"><br>
+[+] ~ Hacked by ./Kecot | KOndom Pecah~ [+]<br><br></font>
+<font size="5" face="Inconsolata" color="gold"> Mwehehehehehehehe
+<br></font>
+
+<font face="Inconsolata" size="5" color="red"><br><br>
+<br>We Are: Kecot Cyber Team 
+ </font><br><br><br><br>
